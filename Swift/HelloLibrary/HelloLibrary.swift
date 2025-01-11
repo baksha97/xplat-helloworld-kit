ce1660904d34f68db917abcd4322d313b9f64576
Alfayed Baksh <baksha97@gmail.com>
2025-01-11 09:09:52 -0500

    init

---
+++ Swift/HelloLibrary/HelloLibrary.swift
@@ -0,0 +1,9 @@
+// Swift/HelloLibrary/HelloLibrary.swift
+
+public struct Greeter {
+    public init() {}
+    
+    public func greet() -> String {
+        return "Hello, World!"
+    }
+}
---
+++ Swift/HelloWorld/main.swift
@@ -0,0 +1,6 @@
+// Swift/HelloWorld/main.swift
+
+import HelloLibrary
+
+let greeter = Greeter()
+print(greeter.greet())
